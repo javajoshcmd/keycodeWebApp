3278ea9cdb4b559939a3e1fc6407d9bbcc60b404
Josh Howcroft <Josh@Joshs-iMac.local>
2021-04-19 16:47:06 +0100

    added shift key functionality

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 
-  window.addEventListener("keypress", () => {
+  window.addEventListener("keydown", () => {
     document.getElementById("header").innerHTML = `<p> key: ${event.key}</p>`;
     document.getElementById("eventCode").innerHTML = `<p> Key code: ${event.keyCode}</p>`;
     document.getElementById("eventWhich").innerHTML = `<p> Event code: ${event.code}</p>`;
