45b6ba6019af117ea7baa2cb7775ac833f442b93
Josh Howcroft <Josh@Joshs-iMac.local>
2021-04-19 22:57:17 +0100

    changed style to dark theme

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
   window.addEventListener("keydown", () => {
-    document.getElementById("header").innerHTML = `<p> key: ${event.key}</p>`;
+    document.getElementById("header").innerHTML = `<p> key pressed: ${event.key}</p>`;
     document.getElementById("eventCode").innerHTML = `<p> Key code: ${event.keyCode}</p>`;
     document.getElementById("eventWhich").innerHTML = `<p> Event code: ${event.code}</p>`;
 })
